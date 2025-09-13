ed5e084488b5aa8dc20e0257c924699cc85488ed
Arpit Dhote <arpitdhote51@gmail.com>
2025-09-13 08:16:58 +0000

    Add the Google Analytics SDK Now that Google Analytics is enabled, you s

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 import { getAuth, type Auth } from "firebase/auth";
 import { initializeFirestore, persistentLocalCache, type Firestore } from "firebase/firestore";
+import { getAnalytics, type Analytics } from "firebase/analytics";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDcFJTJnGLI-uVStqI8uuQVcQMY34ilMJg",
@@ -9,29 +10,27 @@ const firebaseConfig = {
     storageBucket: "studio-7376954909-7abc4.firebasestorage.app",
     messagingSenderId: "131083878984",
     appId: "1:131083878984:web:4504e29ddc8d7b405e4e7d",
-    measurementId: ""
+    measurementId: "G-GDEGN7X24J"
 };
 
 let app: FirebaseApp;
 let auth: Auth;
 let db: Firestore;
+let analytics: Analytics;
 
 // Initialize Firebase on the client side
 if (typeof window !== 'undefined') {
     if (!getApps().length) {
         app = initializeApp(firebaseConfig);
-        auth = getAuth(app);
-        db = initializeFirestore(app, {
-            localCache: persistentLocalCache({})
-        });
     } else {
         app = getApp();
-        auth = getAuth(app);
-        db = initializeFirestore(app, {
-            localCache: persistentLocalCache({})
-        });
     }
+    auth = getAuth(app);
+    db = initializeFirestore(app, {
+        localCache: persistentLocalCache({})
+    });
+    analytics = getAnalytics(app);
 }
 
 // Export the initialized instances
-export { app, auth, db };
+export { app, auth, db, analytics };
