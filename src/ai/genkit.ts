0c7d69c483e7edb20014ae04defc490892dd89ec
Arpit Dhote <arpitdhote51@gmail.com>
2025-09-13 14:16:27 +0000

    okay i dont want to add ai monitor

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,7 +3,7 @@ import {googleAI} from '@genkit-ai/googleai';
 import {firebase} from '@genkit-ai/firebase';
 
 export const ai = genkit({
-  plugins: [googleAI(), firebase()],
+  plugins: [googleAI()],
   model: 'googleai/gemini-2.5-flash',
   enableTracingAndMetrics: true,
 });
